f27bfa504c552037b456e08bbdd1226866d10e87
Stiven1621 <stiven.cor2002@gmai.com>
2020-12-08 16:56:50 -0500

    Juego finalizado

--- public/sonido.js
+++
@@ -1,9 +0,0 @@
-function cargar(){
-    const sonidos = document.getElementById("sonidos");
-    document.addEventListener('load', function(evento){
-        if(evento.keyCode == 32)
-        {
-            sonidos.innerHTML = '<audio src="fondo.mp3" controls></audio>'
-        }
-    })
-}
